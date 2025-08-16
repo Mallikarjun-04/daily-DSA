e215fabac61542041018530043ae5f90f8df2081
Mallikarjun-04 <mallikarjundvs04@gmail.com>
2025-08-16 23:17:55 +0530

    Added leetcode-217 to HashMap folder

---
+++ HashMap/leetcode-217.py
@@ -0,0 +1,54 @@
+'''
+@leetcode-217. Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+
+'''
+
+def containsDuplicate(nums):
+        diki={}
+        for i in nums:
+            if i in diki:
+                diki[i]+=1
+            else:
+                diki[i]=1
+        for i in diki:
+            if diki[i]>1:
+                return True
+        else:
+            return False
+nums=list(map(int,input().split()))
+print(containsDuplicate(nums))
