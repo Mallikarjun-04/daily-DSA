741f6d7ca33645560efef9c7b0057062e5704c24
Mallikarjun-04 <mallikarjundvs04@gmail.com>
2025-08-31 09:33:44 +0530

    Added leetcode-1732 to Arrays folder

---
+++ HashMap/leetcode-1636.py
@@ -0,0 +1,50 @@
+'''
+@leetcode-1636. Sort Array by Increasing Frequency
+
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+
+ 
+Example 1:
+
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+Example 2:
+
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+Example 3:
+
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+
+'''
+
+def frequencySort(nums):
+        diki={}
+        for i in nums:
+            if i not in diki:
+                diki[i]=1
+            else:
+                diki[i]+=1
+        #print(diki)
+        dik= {key: diki[key] for key in sorted(diki,reverse=True)}
+        #print(dik)
+        dici = dict(sorted(dik.items(), key=lambda item: item[1]))
+        #print(dici)
+        ans=[]
+        for i in dici:
+            for j in range(dici[i]):
+                ans.append(i)
+        return(ans)
+nums=list(map(int,input().split()))
+print(frequencySort(nums))
