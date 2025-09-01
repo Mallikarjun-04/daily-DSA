a05783e02dde066301254430cf01272e895a0c30
Mallikarjun-04 <mallikarjundvs04@gmail.com>
2025-09-01 18:40:07 +0530

    Added leetcode-2574 to Arrays folder

---
+++ Arrays/leetcode-2574.py
@@ -0,0 +1,51 @@
+'''
+@leetcode-2574. Left and Right Sum Differences
+
+You are given a 0-indexed integer array nums of size n.
+
+Define two arrays leftSum and rightSum where:
+
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+
+ 
+
+Example 1:
+
+Input: nums = [10,4,8,3]
+Output: [15,1,11,22]
+Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+Example 2:
+
+Input: nums = [1]
+Output: [0]
+Explanation: The array leftSum is [0] and the array rightSum is [0].
+The array answer is [|0 - 0|] = [0].
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 10^5
+'''
+
+def leftRightDifference(nums):
+        left=[]
+        right=[]
+        pre=[]
+        temp=0
+        for i in range(len(nums)):
+            left.append(temp)
+            temp=temp+nums[i]
+            ans=0
+            for j in range(i+1,len(nums)):
+                ans=ans+nums[j]
+            right.append(ans)
+        for i in range(len(left)):
+            for j in range(i,i+1):
+                pre.append(abs((left[i]-right[j])))
+        return (pre)
+nums=list(map(int,input().split()))
+print(leftRightDifference(nums))
